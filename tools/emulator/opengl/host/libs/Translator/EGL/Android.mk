android/platform_development

Emulator's host translator EGL implementation.
This is the EGL implementation for the host EGL/GLES translator.

EglImp.cpp - includes the EGL entry points implementation.
ThreadInfo.cpp - defines the per-thread info which is shared to EGL and GLES
EglOsApi.h - platform dependant interface functions.
EglLinuxApi.cpp - implements the GLX platform of EglOsApi
EglWindowsApi.cpp - implenents the Windows platform of EglOsApi - This has not been compiled yet!!!

Change-Id: I30a8cf457858ef04febf67005787d8d18e5e7a70

tools/emulator/opengl/host/libs/Translator/EGL/Android.mk
@@ -1,0 +1,48 @@
+LOCAL_PATH := $(call my-dir)
+
+### EGL host implementation ########################
+include $(CLEAR_VARS)
+
+translator_path := $(LOCAL_PATH)/..
+
+OS_SRCS:=
+
+
+ifeq ($(HOST_OS),linux)
+    OS_SRCS = EglLinuxApi.cpp
+    LOCAL_LDLIBS := -lX11 -lGL -ldl
+endif
+
+ifeq ($(HOST_OS),darwin)
+    OS_SRCS = EglMacApi.cpp
+endif
+
+ifeq ($(HOST_OS),windows)
+    OS_SRCS = EglWindowsApi.cpp
+endif
+
+LOCAL_SRC_FILES :=            \
+     $(OS_SRCS)               \
+     ThreadInfo.cpp           \
+     EglImp.cpp               \
+     EglConfig.cpp            \
+     EglContext.cpp           \
+     EglGlobalInfo.cpp        \
+     EglValidate.cpp          \
+     EglSurface.cpp           \
+     EglWindowSurface.cpp     \
+     EglPbufferSurface.cpp    \
+     EglPixmapSurface.cpp     \
+     EglThreadInfo.cpp        \
+     EglDisplay.cpp
+
+
+LOCAL_C_INCLUDES += \
+                 $(translator_path)/include
+
+LOCAL_CFLAGS := -g -O0
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := libEGL_translator
+LOCAL_STATIC_LIBRARIES := libGLcommon libcutils
+include $(BUILD_HOST_SHARED_LIBRARY)
+

bluestacks/development-a16

tools/emulator/opengl/host/libs/Translator/EGL/Android.mk
@@ -1,0 +1,48 @@
+LOCAL_PATH := $(call my-dir)
+
+### EGL host implementation ########################
+include $(CLEAR_VARS)
+
+translator_path := $(LOCAL_PATH)/..
+
+OS_SRCS:=
+
+
+ifeq ($(HOST_OS),linux)
+    OS_SRCS = EglX11Api.cpp
+    LOCAL_LDLIBS := -lX11 -lGL -ldl
+endif
+
+ifeq ($(HOST_OS),darwin)
+    OS_SRCS = EglMacApi.cpp
+endif
+
+ifeq ($(HOST_OS),windows)
+    OS_SRCS = EglWindowsApi.cpp
+endif
+
+LOCAL_SRC_FILES :=            \
+     $(OS_SRCS)               \
+     ThreadInfo.cpp           \
+     EglImp.cpp               \
+     EglConfig.cpp            \
+     EglContext.cpp           \
+     EglGlobalInfo.cpp        \
+     EglValidate.cpp          \
+     EglSurface.cpp           \
+     EglWindowSurface.cpp     \
+     EglPbufferSurface.cpp    \
+     EglPixmapSurface.cpp     \
+     EglThreadInfo.cpp        \
+     EglDisplay.cpp
+
+
+LOCAL_C_INCLUDES += \
+                 $(translator_path)/include
+
+LOCAL_CFLAGS := -g -O0
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := libEGL_translator
+LOCAL_STATIC_LIBRARIES := libGLcommon libcutils
+include $(BUILD_HOST_SHARED_LIBRARY)
+