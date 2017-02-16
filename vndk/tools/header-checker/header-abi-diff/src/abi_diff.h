android/platform_development

Added header-abi-diff.
header-abi-diff takes two input dumps produced by header-abi-dumper and
produces an abi compatiblity report.

header-abi-dumper, header-abi-linker now produce dumps formatted only in
protobuf TextFormat.

Test: Dump the abi exposed by test/input/exampl1.cpp. Add fields, change
      return types of functions and dump the abi of this version. Produce the
      compatibility report by: header-abi-diff -o <ver1-dump> -b <ver2-dump>
      -c <compatibility-report>.

Change-Id: I5444912d84de726aaa18977ca9162162aa13b760

vndk/tools/header-checker/header-abi-diff/src/abi_diff.h
@@ -1,0 +1,125 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "abi_diff_wrappers.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#include "proto/abi_dump.pb.h"
+#include "proto/abi_diff.pb.h"
+#pragma clang diagnostic pop
+
+#include <memory>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class HeaderAbiDiff {
+ public:
+  enum status {
+    COMPATIBLE = 1 << 0,
+    EXTENSION = 1 << 1,
+    INCOMPATIBLE = 1 << 2,
+  };
+
+
+  HeaderAbiDiff(const std::string &old_dump, const std::string &new_dump,
+                const std::string &compatibility_report)
+      : old_dump_(old_dump), new_dump_(new_dump), cr_(compatibility_report) { };
+
+  status GenerateCompatibilityReport();
+
+ private:
+  status CompareTUs(const abi_dump::TranslationUnit &old_tu,
+                    const abi_dump::TranslationUnit &new_tu);
+  // Collect* methods fill in the diff_tu.
+  status CollectRecords(abi_diff::TranslationUnitDiff *abi_diff,
+                        const abi_dump::TranslationUnit &old_tu,
+                        const abi_dump::TranslationUnit &new_tu);
+
+  status CollectFunctions(abi_diff::TranslationUnitDiff *abi_diff,
+                          const abi_dump::TranslationUnit &old_tu,
+                          const abi_dump::TranslationUnit &new_tu);
+
+  status CollectEnums(abi_diff::TranslationUnitDiff *abi_diff,
+                      const abi_dump::TranslationUnit &old_tu,
+                      const abi_dump::TranslationUnit &new_tu);
+
+
+  template <typename T>
+  inline void AddToMap(std::map<std::string, const T *> *dst,
+                       const google::protobuf::RepeatedPtrField<T> &src);
+
+  template <typename T>
+  bool PopulateRemovedElements(
+      google::protobuf::RepeatedPtrField<T> *dst,
+      const std::map<std::string, const T *> &old_elements_map,
+      const std::map<std::string, const T *> &new_elements_map) const;
+
+  template <typename T, typename TDiff>
+  bool PopulateCommonElements(
+      google::protobuf::RepeatedPtrField<TDiff> *dst,
+      const std::map<std::string, const T *> &old_elements_map,
+      const std::map<std::string, const T *> &new_elements_map) const;
+
+  template <typename T, typename TDiff>
+  bool DumpDiffElements(
+      google::protobuf::RepeatedPtrField<TDiff> *dst,
+      std::vector<std::pair<const T *, const T *>> &pairs) const;
+
+  template <typename T>
+  bool DumpLoneElements(google::protobuf::RepeatedPtrField<T> *dst,
+                        std::vector<const T *> &elements) const;
+
+ private:
+  const std::string &old_dump_;
+  const std::string &new_dump_;
+  const std::string &cr_;
+
+  // HashMaps for the old tu abis
+  std::map<std::string, const abi_dump::RecordDecl *> old_dump_records_;
+  std::map<std::string, const abi_dump::FunctionDecl *> old_dump_functions_;
+  std::map<std::string, const abi_dump::EnumDecl *> old_dump_enums_;
+
+  // HashMaps for the new tu abis
+  std::map<std::string, const abi_dump::RecordDecl *> new_dump_records_;
+  std::map<std::string, const abi_dump::FunctionDecl *> new_dump_functions_;
+  std::map<std::string, const abi_dump::EnumDecl *> new_dump_enums_;
+};
+
+typedef HeaderAbiDiff::status status;
+
+template <typename T>
+inline void HeaderAbiDiff::AddToMap(
+    std::map<std::string, const T *> *dst,
+    const google::protobuf::RepeatedPtrField<T> &src) {
+  for (auto &&element : src) {
+    dst->insert(std::make_pair(element.linker_set_key(), &element));
+  }
+}
+
+static inline status operator|(status f, status s) {
+  return static_cast<status>(
+      static_cast<std::underlying_type<status>::type>(f) |
+      static_cast<std::underlying_type<status>::type>(s));
+}
+
+static inline status operator&(status f, status s) {
+  return static_cast<status>(
+      static_cast<std::underlying_type<status>::type>(f) &
+      static_cast<std::underlying_type<status>::type>(s));
+}

vndk/tools/header-checker/header-abi-dumper/src/abi_wrappers.cpp
@@ -225,9 +225,13 @@ bool RecordDeclWrapper::SetupRecordFields(
                    << " to reference dump\n";
       return false;
     }
-    record_fieldp->set_field_name(field->getName());
-    record_fieldp->set_field_type(QualTypeToString(field->getType()));
-    record_fieldp->set_access(AccessToString(field->getAccess()));
+    std::string name = field->getName();
+    std::string type = QualTypeToString(field->getType());
+    std::string access = AccessToString(field->getAccess());
+    record_fieldp->set_field_name(name);
+    record_fieldp->set_field_type(type);
+    record_fieldp->set_access(access);
+    record_fieldp->set_linker_set_key(name + type + access);
     field++;
   }
   return true;
@@ -248,11 +252,14 @@ bool RecordDeclWrapper::SetupCXXBases(abi_dump::RecordDecl *cxxp) const {
       llvm::errs() << " Couldn't add base specifier to reference dump\n";
       return false;
     }
-    base_specifierp->set_fully_qualified_name(
-        QualTypeToString(base_class->getType()));
-    base_specifierp->set_is_virtual(base_class->isVirtual());
-    base_specifierp->set_access(
-        AccessToString(base_class->getAccessSpecifier()));
+    std::string name = QualTypeToString(base_class->getType());
+    bool is_virtual = base_class->isVirtual();
+    char is_virtual_c = is_virtual ? 't' : 'f';
+    std::string access = AccessToString(base_class->getAccessSpecifier());
+    base_specifierp->set_fully_qualified_name(name);
+    base_specifierp->set_is_virtual(is_virtual);
+    base_specifierp->set_access(access);
+    base_specifierp->set_linker_set_key(name + is_virtual_c + access);
     base_class++;
   }
   return true;
@@ -298,9 +305,10 @@ bool RecordDeclWrapper::SetupTemplateInfo(
 void RecordDeclWrapper::SetupRecordInfo(abi_dump::RecordDecl *record_declp,
                                         const std::string &source_file) const {
   std::string qualified_name = GetTagDeclQualifiedName(record_decl_);
+  std::string mangled_name = GetMangledNameDecl(record_decl_);
   record_declp->set_fully_qualified_name(qualified_name);
-  //TODO: Add Template Information
-  record_declp->set_linker_set_key(qualified_name);
+  record_declp->set_mangled_record_name(mangled_name);
+  record_declp->set_linker_set_key(mangled_name);
   record_declp->set_source_file(source_file);
   record_declp->set_access(AccessToString(record_decl_->getAccess()));
 }

bluestacks/development-a16

Test: Dump the abi exposed by test/input/example1.cpp. Add fields, change
      compatibility report by: header-abi-diff -old <ver1-dump> -new <ver2-dump>
      -o <compatibility-report>.
      Also tested changing the abi of libjpeg, and comparing a reference
      dump with the new dump. An Abi breakage is reported.
Bug: 32750600

vndk/tools/header-checker/header-abi-diff/src/abi_diff.h
@@ -1,0 +1,125 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "abi_diff_wrappers.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#include "proto/abi_dump.pb.h"
+#include "proto/abi_diff.pb.h"
+#pragma clang diagnostic pop
+
+#include <memory>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class HeaderAbiDiff {
+ public:
+  enum Status {
+    COMPATIBLE = 1 << 0,
+    EXTENSION = 1 << 1,
+    INCOMPATIBLE = 1 << 2,
+  };
+
+
+  HeaderAbiDiff(const std::string &old_dump, const std::string &new_dump,
+                const std::string &compatibility_report)
+      : old_dump_(old_dump), new_dump_(new_dump), cr_(compatibility_report) { }
+
+  Status GenerateCompatibilityReport();
+
+ private:
+  Status CompareTUs(const abi_dump::TranslationUnit &old_tu,
+                    const abi_dump::TranslationUnit &new_tu);
+  // Collect* methods fill in the diff_tu.
+  Status CollectRecords(abi_diff::TranslationUnitDiff *abi_diff,
+                        const abi_dump::TranslationUnit &old_tu,
+                        const abi_dump::TranslationUnit &new_tu);
+
+  Status CollectFunctions(abi_diff::TranslationUnitDiff *abi_diff,
+                          const abi_dump::TranslationUnit &old_tu,
+                          const abi_dump::TranslationUnit &new_tu);
+
+  Status CollectEnums(abi_diff::TranslationUnitDiff *abi_diff,
+                      const abi_dump::TranslationUnit &old_tu,
+                      const abi_dump::TranslationUnit &new_tu);
+
+
+  template <typename T>
+  inline void AddToMap(std::map<std::string, const T *> *dst,
+                       const google::protobuf::RepeatedPtrField<T> &src);
+
+  template <typename T>
+  bool PopulateRemovedElements(
+      google::protobuf::RepeatedPtrField<T> *dst,
+      const std::map<std::string, const T *> &old_elements_map,
+      const std::map<std::string, const T *> &new_elements_map) const;
+
+  template <typename T, typename TDiff>
+  bool PopulateCommonElements(
+      google::protobuf::RepeatedPtrField<TDiff> *dst,
+      const std::map<std::string, const T *> &old_elements_map,
+      const std::map<std::string, const T *> &new_elements_map) const;
+
+  template <typename T, typename TDiff>
+  bool DumpDiffElements(
+      google::protobuf::RepeatedPtrField<TDiff> *dst,
+      std::vector<std::pair<const T *, const T *>> &pairs) const;
+
+  template <typename T>
+  bool DumpLoneElements(google::protobuf::RepeatedPtrField<T> *dst,
+                        std::vector<const T *> &elements) const;
+
+ private:
+  const std::string &old_dump_;
+  const std::string &new_dump_;
+  const std::string &cr_;
+
+  // HashMaps for the old tu abis
+  std::map<std::string, const abi_dump::RecordDecl *> old_dump_records_;
+  std::map<std::string, const abi_dump::FunctionDecl *> old_dump_functions_;
+  std::map<std::string, const abi_dump::EnumDecl *> old_dump_enums_;
+
+  // HashMaps for the new tu abis
+  std::map<std::string, const abi_dump::RecordDecl *> new_dump_records_;
+  std::map<std::string, const abi_dump::FunctionDecl *> new_dump_functions_;
+  std::map<std::string, const abi_dump::EnumDecl *> new_dump_enums_;
+};
+
+typedef HeaderAbiDiff::Status Status;
+
+template <typename T>
+inline void HeaderAbiDiff::AddToMap(
+    std::map<std::string, const T *> *dst,
+    const google::protobuf::RepeatedPtrField<T> &src) {
+  for (auto &&element : src) {
+    dst->insert(std::make_pair(element.linker_set_key(), &element));
+  }
+}
+
+static inline Status operator|(Status f, Status s) {
+  return static_cast<Status>(
+      static_cast<std::underlying_type<Status>::type>(f) |
+      static_cast<std::underlying_type<Status>::type>(s));
+}
+
+static inline Status operator&(Status f, Status s) {
+  return static_cast<Status>(
+      static_cast<std::underlying_type<Status>::type>(f) &
+      static_cast<std::underlying_type<Status>::type>(s));
+}

vndk/tools/header-checker/header-abi-dumper/src/abi_wrappers.cpp
@@ -225,9 +225,13 @@ bool RecordDeclWrapper::SetupRecordFields(
                    << " to reference dump\n";
       return false;
     }
-    record_fieldp->set_field_name(field->getName());
-    record_fieldp->set_field_type(QualTypeToString(field->getType()));
-    record_fieldp->set_access(AccessToString(field->getAccess()));
+    std::string name = field->getName();
+    std::string type = QualTypeToString(field->getType());
+    std::string access = AccessToString(field->getAccess());
+    record_fieldp->set_field_name(name);
+    record_fieldp->set_field_type(type);
+    record_fieldp->set_access(access);
+    record_fieldp->set_linker_set_key(name + type + access);
     field++;
   }
   return true;
@@ -248,11 +252,14 @@ bool RecordDeclWrapper::SetupCXXBases(abi_dump::RecordDecl *cxxp) const {
       llvm::errs() << " Couldn't add base specifier to reference dump\n";
       return false;
     }
-    base_specifierp->set_fully_qualified_name(
-        QualTypeToString(base_class->getType()));
-    base_specifierp->set_is_virtual(base_class->isVirtual());
-    base_specifierp->set_access(
-        AccessToString(base_class->getAccessSpecifier()));
+    std::string name = QualTypeToString(base_class->getType());
+    bool is_virtual = base_class->isVirtual();
+    char is_virtual_c = is_virtual ? 't' : 'f';
+    std::string access = AccessToString(base_class->getAccessSpecifier());
+    base_specifierp->set_fully_qualified_name(name);
+    base_specifierp->set_is_virtual(is_virtual);
+    base_specifierp->set_access(access);
+    base_specifierp->set_linker_set_key(name + is_virtual_c + access);
     base_class++;
   }
   return true;
@@ -298,9 +305,11 @@ bool RecordDeclWrapper::SetupTemplateInfo(
 void RecordDeclWrapper::SetupRecordInfo(abi_dump::RecordDecl *record_declp,
                                         const std::string &source_file) const {
   std::string qualified_name = GetTagDeclQualifiedName(record_decl_);
+  std::string mangled_name = GetMangledNameDecl(record_decl_);
+  std::string linker_key = (mangled_name == "") ? qualified_name : mangled_name;
   record_declp->set_fully_qualified_name(qualified_name);
-  //TODO: Add Template Information
-  record_declp->set_linker_set_key(qualified_name);
+  record_declp->set_mangled_record_name(mangled_name);
+  record_declp->set_linker_set_key(linker_key);
   record_declp->set_source_file(source_file);
   record_declp->set_access(AccessToString(record_decl_->getAccess()));
 }