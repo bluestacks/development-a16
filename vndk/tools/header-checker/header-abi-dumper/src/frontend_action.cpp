android/platform_development

Allow symlinks to be included while collecting exported headers.
Bug: 62380238

Also:
1) Some code clean up to avoid copying set of exported headers.

Test: The following:
      1) change build/soong/cc/builder.go to use built tool instead of prebuilt.
      2) make liblog/include/log.h a symlink to a moved copy -> liblog/log.h.
      3) mm -j64 in liblog, liblog.so.lsdump has enum android_LogPriority, which
         is not present without this change.

Change-Id: Iaecfad5b638ee0db8343bbd80eb60c6aa8f1feac

vndk/tools/header-checker/header-abi-dumper/src/frontend_action.cpp
@@ -25,16 +25,13 @@
 #include <llvm/Support/Path.h>
 
 HeaderCheckerFrontendAction::HeaderCheckerFrontendAction(
-    const std::string &dump_name, const std::vector<std::string> &exports)
-  : dump_name_(dump_name), exported_header_dirs_(exports) { }
+    const std::string &dump_name, const std::set<std::string> &exports)
+  : dump_name_(dump_name), exported_headers_(exports) { }
 
 std::unique_ptr<clang::ASTConsumer>
 HeaderCheckerFrontendAction::CreateASTConsumer(clang::CompilerInstance &ci,
                                                llvm::StringRef header_file) {
-  std::set<std::string> exported_headers;
-  exported_headers = abi_util::CollectAllExportedHeaders(exported_header_dirs_);
   // Create AST consumers.
-  return llvm::make_unique<HeaderASTConsumer>(header_file,
-                                              &ci, dump_name_,
-                                              exported_headers);
+  return llvm::make_unique<HeaderASTConsumer>(header_file, &ci, dump_name_,
+                                              exported_headers_);
 }

vndk/tools/header-checker/header-abi-dumper/src/frontend_action.h
@@ -31,12 +31,12 @@ namespace clang {
 class HeaderCheckerFrontendAction : public clang::ASTFrontendAction {
  private:
   std::string dump_name_;
-  const std::vector<std::string> &exported_header_dirs_;
+  const std::set<std::string> &exported_headers_;
 
  public:
   HeaderCheckerFrontendAction(
       const std::string &dump_name,
-      const std::vector<std::string> &export_header_dirs);
+      const std::set<std::string> &export_headers);
 
  protected:
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(

bluestacks/development-a16

vndk/tools/header-checker/header-abi-dumper/src/frontend_action.cpp
@@ -25,16 +25,13 @@
 #include <llvm/Support/Path.h>
 
 HeaderCheckerFrontendAction::HeaderCheckerFrontendAction(
-    const std::string &dump_name, const std::vector<std::string> &exports)
-  : dump_name_(dump_name), exported_header_dirs_(exports) { }
+    const std::string &dump_name, const std::set<std::string> &exported_headers)
+  : dump_name_(dump_name), exported_headers_(exported_headers) { }
 
 std::unique_ptr<clang::ASTConsumer>
 HeaderCheckerFrontendAction::CreateASTConsumer(clang::CompilerInstance &ci,
                                                llvm::StringRef header_file) {
-  std::set<std::string> exported_headers;
-  exported_headers = abi_util::CollectAllExportedHeaders(exported_header_dirs_);
   // Create AST consumers.
-  return llvm::make_unique<HeaderASTConsumer>(header_file,
-                                              &ci, dump_name_,
-                                              exported_headers);
+  return llvm::make_unique<HeaderASTConsumer>(header_file, &ci, dump_name_,
+                                              exported_headers_);
 }

vndk/tools/header-checker/header-abi-dumper/src/frontend_action.h
@@ -31,12 +31,12 @@ namespace clang {
 class HeaderCheckerFrontendAction : public clang::ASTFrontendAction {
  private:
   std::string dump_name_;
-  const std::vector<std::string> &exported_header_dirs_;
+  const std::set<std::string> &exported_headers_;
 
  public:
   HeaderCheckerFrontendAction(
       const std::string &dump_name,
-      const std::vector<std::string> &export_header_dirs);
+      const std::set<std::string> &exported_headers);
 
  protected:
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(