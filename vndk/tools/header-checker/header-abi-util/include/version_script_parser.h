android/platform_development

header-abi-linker: Support solib and version script
This commit allows user to specify both `-so` (for a shared object file)
and `-v` (for a version script).  If both of them are specified, the
generated output will only include the symbols that are marked as
exported in both cases.

This commit also rewritten the version script parser so that it can
work with real version script cases.  This includes how the parser
skips a comment line.

Furthermore, the version script parser accepts a list of excluded symbol
versions and symbol tags that should not be included in the generated
output.  To customize the behavior, pass `--exclude-symbol-version`
and/or `--exclude-symbol-tag` to `header-abi-linker`.

Bug: 122845490
Test: ./tests/test.py
Test: out/host/linux-x86/nativetest64/libheader-abi-util_test/\
      libheader-abi-util_test
Change-Id: Ibf048bba6ccd5ac44f265d1fe767c2ded3d13a8d

vndk/tools/header-checker/header-abi-util/include/version_script_parser.h
@@ -15,64 +15,116 @@
 #ifndef VERSION_SCRIPT_PARSER_H_
 #define VERSION_SCRIPT_PARSER_H_
 
+#include "api_level.h"
+#include "exported_symbol_set.h"
 #include "ir_representation.h"
 
 #include <map>
 #include <set>
 #include <string>
 
+
 namespace abi_util {
 
+
 class VersionScriptParser {
- public:
-  enum LineScope {
-    global,
-    local,
+ private:
+  enum class LineScope {
+    GLOBAL,
+    LOCAL,
   };
 
-  VersionScriptParser(const std::string &version_script,
-                      const std::string &arch,
-                      const std::string &api);
-  bool Parse();
 
-  const std::map<std::string, ElfFunctionIR> &GetFunctions();
+  struct ParsedTags {
+   public:
+    unsigned has_arch_tags_ : 1;
+    unsigned has_current_arch_tag_ : 1;
+    unsigned has_introduced_tags_ : 1;
+    unsigned has_excluded_tags_ : 1;
+    unsigned has_future_tag_ : 1;
+    unsigned has_var_tag_ : 1;
+    ApiLevel introduced_;
 
-  const std::map<std::string, ElfObjectIR> &GetGlobVars();
 
-  const std::set<std::string> &GetFunctionRegexs();
+   public:
+    ParsedTags()
+        : has_arch_tags_(0), has_current_arch_tag_(0), has_introduced_tags_(0),
+          has_excluded_tags_(0), has_future_tag_(0), has_var_tag_(0),
+          introduced_(-1) {}
+  };
 
-  const std::set<std::string> &GetGlobVarRegexs();
+
+ public:
+  class ErrorHandler {
+   public:
+    virtual ~ErrorHandler();
+
+    virtual void OnError(int line_no, const std::string &error_msg) = 0;
+  };
+
+
+ public:
+  VersionScriptParser();
+
+  void SetArch(const std::string &arch);
+
+  void SetApiLevel(ApiLevel api_level) {
+    api_level_ = api_level;
+  }
+
+  void AddExcludedSymbolVersion(const std::string &version) {
+    excluded_symbol_versions_.insert(version);
+  }
+
+  void AddExcludedSymbolTag(const std::string &tag) {
+    excluded_symbol_tags_.insert(tag);
+  }
+
+  void SetErrorHandle(std::unique_ptr<ErrorHandler> error_handler) {
+    error_handler_ = std::move(error_handler);
+  }
+
+  std::unique_ptr<ExportedSymbolSet> Parse(std::istream &version_script_stream);
+
 
  private:
-  bool ParseInnerBlock(std::ifstream &symbol_ifstream);
+  bool ReadLine(std::string &line);
 
-  LineScope GetLineScope(std::string &line, LineScope scope);
+  bool ParseVersionBlock(bool ignore_symbols);
 
-  bool ParseSymbolLine(const std::string &line);
+  bool ParseSymbolLine(const std::string &line, bool is_cpp_symbol);
 
-  bool SymbolInArchAndApiVersion(const std::string &line,
-                                 const std::string &arch, int api);
+  ParsedTags ParseSymbolTags(const std::string &line);
 
-  bool SymbolExported(const std::string &line, const std::string &arch,
-                      int api);
+  bool IsSymbolExported(const ParsedTags &tags);
 
-  int ApiStrToInt(const std::string &api);
-
-  void AddToVars(std::string &symbol);
-
-  void AddToFunctions(std::string &symbol);
 
  private:
-  const std::string &version_script_;
-  const std::string &arch_;
-  std::map<std::string, ElfFunctionIR> functions_;
-  std::map<std::string, ElfObjectIR> globvars_;
-  // Added to speed up version script parsing and linking.
-  std::set<std::string> function_regexs_;
-  std::set<std::string> globvar_regexs_;
-  int api_;
+  void ReportError(const std::string &error_msg) {
+    if (error_handler_) {
+      error_handler_->OnError(line_no_, error_msg);
+    }
+  }
+
+
+ private:
+  std::unique_ptr<ErrorHandler> error_handler_;
+
+  std::string arch_;
+  std::string introduced_arch_tag_;
+  ApiLevel api_level_;
+
+  std::set<std::string> excluded_symbol_versions_;
+  std::set<std::string> excluded_symbol_tags_;
+
+  std::istream *stream_;
+  int line_no_;
+
+  std::unique_ptr<ExportedSymbolSet> exported_symbols_;
 };
 
+
 }  // namespace abi_util
+
 
 #endif  // VERSION_SCRIPT_PARSER_H_

bluestacks/development-a16

Test: atest  # under development/vndk/tools/header-checker

vndk/tools/header-checker/header-abi-util/include/version_script_parser.h
@@ -15,64 +15,117 @@
 #ifndef VERSION_SCRIPT_PARSER_H_
 #define VERSION_SCRIPT_PARSER_H_
 
+#include "api_level.h"
+#include "exported_symbol_set.h"
 #include "ir_representation.h"
 
+#include <functional>
 #include <map>
 #include <set>
 #include <string>
 
+
 namespace abi_util {
 
+
 class VersionScriptParser {
- public:
-  enum LineScope {
-    global,
-    local,
+ private:
+  enum class LineScope {
+    GLOBAL,
+    LOCAL,
   };
 
-  VersionScriptParser(const std::string &version_script,
-                      const std::string &arch,
-                      const std::string &api);
-  bool Parse();
 
-  const std::map<std::string, ElfFunctionIR> &GetFunctions();
+  struct ParsedTags {
+   public:
+    unsigned has_arch_tags_ : 1;
+    unsigned has_current_arch_tag_ : 1;
+    unsigned has_introduced_tags_ : 1;
+    unsigned has_excluded_tags_ : 1;
+    unsigned has_future_tag_ : 1;
+    unsigned has_var_tag_ : 1;
+    ApiLevel introduced_;
 
-  const std::map<std::string, ElfObjectIR> &GetGlobVars();
 
-  const std::set<std::string> &GetFunctionRegexs();
+   public:
+    ParsedTags()
+        : has_arch_tags_(0), has_current_arch_tag_(0), has_introduced_tags_(0),
+          has_excluded_tags_(0), has_future_tag_(0), has_var_tag_(0),
+          introduced_(-1) {}
+  };
 
-  const std::set<std::string> &GetGlobVarRegexs();
+
+ public:
+  class ErrorHandler {
+   public:
+    virtual ~ErrorHandler();
+
+    virtual void OnError(int line_no, const std::string &error_msg) = 0;
+  };
+
+
+ public:
+  VersionScriptParser();
+
+  void SetArch(const std::string &arch);
+
+  void SetApiLevel(ApiLevel api_level) {
+    api_level_ = api_level;
+  }
+
+  void AddExcludedSymbolVersion(const std::string &version) {
+    excluded_symbol_versions_.insert(version);
+  }
+
+  void AddExcludedSymbolTag(const std::string &tag) {
+    excluded_symbol_tags_.insert(tag);
+  }
+
+  void SetErrorHandler(std::unique_ptr<ErrorHandler> error_handler) {
+    error_handler_ = std::move(error_handler);
+  }
+
+  std::unique_ptr<ExportedSymbolSet> Parse(std::istream &version_script_stream);
+
 
  private:
-  bool ParseInnerBlock(std::ifstream &symbol_ifstream);
+  bool ReadLine(std::string &line);
 
-  LineScope GetLineScope(std::string &line, LineScope scope);
+  bool ParseVersionBlock(bool ignore_symbols);
 
-  bool ParseSymbolLine(const std::string &line);
+  bool ParseSymbolLine(const std::string &line, bool is_cpp_symbol);
 
-  bool SymbolInArchAndApiVersion(const std::string &line,
-                                 const std::string &arch, int api);
+  ParsedTags ParseSymbolTags(const std::string &line);
 
-  bool SymbolExported(const std::string &line, const std::string &arch,
-                      int api);
+  bool IsSymbolExported(const ParsedTags &tags);
 
-  int ApiStrToInt(const std::string &api);
-
-  void AddToVars(std::string &symbol);
-
-  void AddToFunctions(std::string &symbol);
 
  private:
-  const std::string &version_script_;
-  const std::string &arch_;
-  std::map<std::string, ElfFunctionIR> functions_;
-  std::map<std::string, ElfObjectIR> globvars_;
-  // Added to speed up version script parsing and linking.
-  std::set<std::string> function_regexs_;
-  std::set<std::string> globvar_regexs_;
-  int api_;
+  void ReportError(const std::string &error_msg) {
+    if (error_handler_) {
+      error_handler_->OnError(line_no_, error_msg);
+    }
+  }
+
+
+ private:
+  std::unique_ptr<ErrorHandler> error_handler_;
+
+  std::string arch_;
+  std::string introduced_arch_tag_;
+  ApiLevel api_level_;
+
+  std::set<std::string, std::less<>> excluded_symbol_versions_;
+  std::set<std::string, std::less<>> excluded_symbol_tags_;
+
+  std::istream *stream_;
+  int line_no_;
+
+  std::unique_ptr<ExportedSymbolSet> exported_symbols_;
 };
 
+
 }  // namespace abi_util
+
 
 #endif  // VERSION_SCRIPT_PARSER_H_