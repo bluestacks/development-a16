android/platform_development

Add support for partial update and target library.
New functions:
- The uploaded target files will be stored into a database.
- The detailed info of target files can be shown in the target library.
- Add support for generating partial OTA.

Test: Mannual tested.
Change-Id: Idf715994fe51b9ab6addbfe72792be890243702d

tools/otagui/src/components/FileSelect.vue
@@ -1,0 +1,39 @@
+<template>
+  <label v-if="label"> {{ label }} </label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="$attrs"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option
+      v-for="option in options"
+      :key="option.FileName"
+      :value="option.Path"
+      :selected="option.Path === modelValue"
+    >
+      {{ option.FileName }}
+    </option>
+  </select>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+}
+</script>

tools/otagui/src/views/SimpleForm.vue
@@ -3,16 +3,16 @@
     <form @submit.prevent="sendForm">
       <UploadFile @file-uploaded="fetchTargetList" />
       <br>
-      <BaseSelect
+      <FileSelect
         v-if="input.incrementalStatus"
         v-model="input.incremental"
         label="Select the source file"
-        :options="targetList"
+        :options="targetDetails"
       />
-      <BaseSelect
+      <FileSelect
         v-model="input.target"
         label="Select the target file"
-        :options="targetList"
+        :options="targetDetails"
       />
       <button
         type="button"
@@ -24,12 +24,39 @@
         <BaseCheckbox
           v-model="input.verbose"
           :label="'Verbose'"
-        />
-        &emsp;
+        /> &emsp;
         <BaseCheckbox
           v-model="input.incrementalStatus"
           :label="'Incremental'"
         />
+      </div>
+      <div>
+        <BaseCheckbox
+          v-model="input.partialStatus"
+          :label="'Partial'"
+        />
+        <ul v-if="input.partialStatus">
+          <li
+            v-for="partition in updatePartition"
+            :key="partition"
+          > 
+            <button
+              type="button"
+              @click="addPartition(partition)"
+            >
+              Add: {{ partition }}
+            </button> &emsp;
+            <button
+              type="button"
+              @click="deletePartition(partition)"
+            >
+              Minus: {{ partition }}
+            </button>
+          </li>
+          <p v-if="input.partialStatus">
+            Partial List: {{ input.partial }}
+          </p>
+        </ul>
       </div>
       <br>
       <BaseInput
@@ -42,12 +69,42 @@
       </button>
     </form>
   </div>
+  <div>
+    <ul>
+      <h4> Build Library </h4>
+      <li
+        v-for="targetDetail in targetDetails"
+        :key="targetDetail.FileName"
+      >
+        <div>
+          <h5>Build File Name: {{ targetDetail.FileName }}</h5>
+          Uploaded time: {{ formDate(targetDetail.UploadTime) }}
+          <br>
+          Build ID: {{ targetDetail.BuildID }}
+          <br>
+          Build Version: {{ targetDetail.BuildVersion }}
+          <br>
+          Build Flavor: {{ targetDetail.BuildFlavor }}
+          <br>
+          <button @click="selectTarget(targetDetail.Path)">
+            Select as Target File
+          </button>  &emsp;
+          <button
+            :disabled="!input.incrementalStatus"
+            @click="selectIncremental(targetDetail.Path)"
+          >
+            Select as Incremental File
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import BaseInput from '@/components/BaseInput.vue'
 import BaseCheckbox from '@/components/BaseCheckbox.vue'
-import BaseSelect from '@/components/BaseSelect.vue'
+import FileSelect from '@/components/FileSelect.vue'
 import ApiService from '../services/ApiService.js'
 import UploadFile from '@/components/UploadFile.vue'
 import { uuid } from 'vue-uuid'
@@ -57,58 +114,62 @@ export default {
     BaseInput,
     BaseCheckbox,
     UploadFile,
-    BaseSelect,
+    FileSelect,
   },
   data() {
     return {
       id: 0,
-      input: {
-        verbose: false,
-        target: '',
-        output: 'output/',
-        incremental: '',
-        incrementalStatus: false,
-        extra: '',
-      },
+      input: {},
       inputs: [],
       response_message: '',
       targetList: [],
+      targetDetails: [],
     }
   },
   computed: {
-    updateOutput() {
-      return 'output/' + String(this.id) + '.zip'
-    },
+    updatePartition() {
+      let target = this.targetDetails.filter(d => d.Path === this.input.target);
+      return JSON.parse(target[0].Partitions)
+    }
   },
   created() {
+    this.resetInput()
     this.fetchTargetList()
     this.updateUUID()
   },
   methods: {
-    sendForm(e) {
-      // console.log(this.input)
-      ApiService.postInput(this.input, this.id)
-        .then((Response) => {
-          this.response_message = Response.data
-          alert(this.response_message)
-        })
-        .catch((err) => {
-          this.response_message = 'Error! ' + err
-        })
+    resetInput() {
       this.input = {
         verbose: false,
         target: '',
         output: 'output/',
         incremental: '',
         incrementalStatus: false,
+        partial: '',
+        partialStatus: false,
         extra: '',
       }
+    },
+    async sendForm(e) {
+      try {
+        let response = await ApiService.postInput(this.input, this.id)
+        this.response_message = response.data
+        alert(this.response_message)
+      } catch (err) {
+        console.log(err)
+      }
+      this.resetInput()
       this.updateUUID()
     },
     async fetchTargetList() {
       try {
         let response = await ApiService.getFileList('/target')
-        this.targetList = response.data
+        this.targetList = []
+        this.targetDetails = []
+        for (let i in response.data) {
+          this.targetList.push(response.data[i].FileName)
+          this.targetDetails.push(response.data[i])
+        }
       } catch (err) {
         console.log('Fetch Error', err)
       }
@@ -117,7 +178,39 @@ export default {
       this.id = uuid.v1()
       this.input.output += String(this.id) + '.zip'
     },
-  },
+    formDate(unixTime) {
+      let formTime = new Date(unixTime * 1000)
+      let date =
+				formTime.getFullYear() +
+				'-' +
+				(formTime.getMonth() + 1) +
+				'-' +
+				formTime.getDate()
+      let time =
+				formTime.getHours() +
+				':' +
+				formTime.getMinutes() +
+				':' +
+				formTime.getSeconds()
+      return date + ' ' + time
+    },
+    selectTarget(path) {
+      this.input.target = path
+    },
+    selectIncremental(path) {
+      this.input.incremental = path
+    },
+    addPartition(partition) {
+      if (! this.input.partial.includes(partition)) {
+        this.input.partial += partition + ' '
+      }
+    },
+    deletePartition(partition) {
+      if (this.input.partial.includes(partition)) {
+        this.input.partial = this.input.partial.replace(partition + ' ', '')
+      }
+    }
+  }
 }
 </script>
 

bluestacks/development-a16

tools/otagui/src/components/FileSelect.vue
@@ -1,0 +1,39 @@
+<template>
+  <label v-if="label"> {{ label }} </label>
+  <select
+    :value="modelValue"
+    class="field"
+    v-bind="$attrs"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option
+      v-for="option in options"
+      :key="option.file_name"
+      :value="option.path"
+      :selected="option.path === modelValue"
+    >
+      {{ option.file_name }}
+    </option>
+  </select>
+</template>
+
+
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>

tools/otagui/src/views/SimpleForm.vue
@@ -3,16 +3,16 @@
     <form @submit.prevent="sendForm">
       <UploadFile @file-uploaded="fetchTargetList" />
       <br>
-      <BaseSelect
-        v-if="input.incrementalStatus"
+      <FileSelect
+        v-if="input.isIncremental"
         v-model="input.incremental"
         label="Select the source file"
-        :options="targetList"
+        :options="targetDetails"
       />
-      <BaseSelect
+      <FileSelect
         v-model="input.target"
         label="Select the target file"
-        :options="targetList"
+        :options="targetDetails"
       />
       <button
         type="button"
@@ -24,12 +24,25 @@
         <BaseCheckbox
           v-model="input.verbose"
           :label="'Verbose'"
-        />
-        &emsp;
+        /> &emsp;
         <BaseCheckbox
-          v-model="input.incrementalStatus"
+          v-model="input.isIncremental"
           :label="'Incremental'"
         />
+      </div>
+      <div>
+        <BaseCheckbox
+          v-model="input.isPartial"
+          :label="'Partial'"
+        />
+        <PartialCheckbox
+          v-if="input.isPartial"
+          v-model="partitionInclude"
+          :labels="updatePartitions"
+        />
+        <div v-if="input.isPartial">
+          Partial list: {{ partitionList }}
+        </div>
       </div>
       <br>
       <BaseInput
@@ -42,14 +55,53 @@
       </button>
     </form>
   </div>
+  <div>
+    <ul>
+      <h4>Build Library</h4>
+      <strong>
+        Careful: Use a same filename will overwrite the original build.
+      </strong>
+      <br>
+      <button @click="updateBuildLib">
+        Refresh the build Library (use with cautions)
+      </button>
+      <li
+        v-for="targetDetail in targetDetails"
+        :key="targetDetail.file_name"
+      >
+        <div>
+          <h5>Build File Name: {{ targetDetail.file_name }}</h5>
+          Uploaded time: {{ formDate(targetDetail.time) }}
+          <br>
+          Build ID: {{ targetDetail.build_id }}
+          <br>
+          Build Version: {{ targetDetail.build_version }}
+          <br>
+          Build Flavor: {{ targetDetail.build_flavor }}
+          <br>
+          <button
+            :disabled="!input.isIncremental"
+            @click="selectIncremental(targetDetail.path)"
+          >
+            Select as Incremental File
+          </button>
+          &emsp;
+          <button @click="selectTarget(targetDetail.path)">
+            Select as Target File
+          </button>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import BaseInput from '@/components/BaseInput.vue'
 import BaseCheckbox from '@/components/BaseCheckbox.vue'
-import BaseSelect from '@/components/BaseSelect.vue'
+import FileSelect from '@/components/FileSelect.vue'
 import ApiService from '../services/ApiService.js'
 import UploadFile from '@/components/UploadFile.vue'
+import PartialCheckbox from '@/components/PartialCheckbox.vue'
 import { uuid } from 'vue-uuid'
 
 export default {
@@ -57,58 +109,76 @@ export default {
     BaseInput,
     BaseCheckbox,
     UploadFile,
-    BaseSelect,
+    FileSelect,
+    PartialCheckbox,
   },
   data() {
     return {
       id: 0,
-      input: {
-        verbose: false,
-        target: '',
-        output: 'output/',
-        incremental: '',
-        incrementalStatus: false,
-        extra: '',
-      },
+      input: {},
       inputs: [],
       response_message: '',
-      targetList: [],
+      targetDetails: [],
+      partitionInclude: new Map(),
     }
   },
   computed: {
-    updateOutput() {
-      return 'output/' + String(this.id) + '.zip'
+    updatePartitions() {
+      let target = this.targetDetails.filter(
+        (d) => d.path === this.input.target
+      )
+      return target[0].partitions
+    },
+    partitionList() {
+      let list = ''
+      for (let [key, value] of this.partitionInclude) {
+        if (value) {
+          list += key + ' '
+        }
+      }
+      return list
+    },
+  },
+  watch: {
+    partitionList: {
+      handler: function () {
+        this.input.partial = this.partitionList
+      },
     },
   },
   created() {
+    this.resetInput()
     this.fetchTargetList()
     this.updateUUID()
   },
   methods: {
-    sendForm(e) {
-      // console.log(this.input)
-      ApiService.postInput(this.input, this.id)
-        .then((Response) => {
-          this.response_message = Response.data
-          alert(this.response_message)
-        })
-        .catch((err) => {
-          this.response_message = 'Error! ' + err
-        })
+    resetInput() {
       this.input = {
         verbose: false,
         target: '',
         output: 'output/',
         incremental: '',
-        incrementalStatus: false,
+        isIncremental: false,
+        partial: '',
+        isPartial: false,
         extra: '',
       }
+    },
+    async sendForm(e) {
+      try {
+        let response = await ApiService.postInput(this.input, this.id)
+        this.response_message = response.data
+        alert(this.response_message)
+      } catch (err) {
+        console.log(err)
+      }
+      this.resetInput()
       this.updateUUID()
     },
     async fetchTargetList() {
       try {
-        let response = await ApiService.getFileList('/target')
-        this.targetList = response.data
+        let response = await ApiService.getFileList('')
+        this.targetDetails = response.data
       } catch (err) {
         console.log('Fetch Error', err)
       }
@@ -116,6 +186,36 @@ export default {
     updateUUID() {
       this.id = uuid.v1()
       this.input.output += String(this.id) + '.zip'
+    },
+    formDate(unixTime) {
+      let formTime = new Date(unixTime * 1000)
+      let date =
+        formTime.getFullYear() +
+        '-' +
+        (formTime.getMonth() + 1) +
+        '-' +
+        formTime.getDate()
+      let time =
+        formTime.getHours() +
+        ':' +
+        formTime.getMinutes() +
+        ':' +
+        formTime.getSeconds()
+      return date + ' ' + time
+    },
+    selectTarget(path) {
+      this.input.target = path
+    },
+    selectIncremental(path) {
+      this.input.incremental = path
+    },
+    async updateBuildLib() {
+      try {
+        let response = await ApiService.getFileList('/target')
+        this.targetDetails = response.data
+      } catch (err) {
+        console.log('Fetch Error', err)
+      }
     },
   },
 }