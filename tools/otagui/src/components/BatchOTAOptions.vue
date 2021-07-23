Madeinmisery/Keyboard

Add batch generation to OTAGUI.
Batch generation of OTA packages is a important feature
requested by googler and partners: please refer to go/ota-dashboard-doc.
Given n incremental source builds and m target builds,
batch generation will generate n x m OTA packages in total.
If n=0, full OTA package will be generated.

The front end will be taking in the source/target lists and send the
request to backend one-by-one.

Test: mannual tested.

Change-Id: I769359ee69c7aa8c71704c4e119c374635554dfb

tools/otagui/src/components/BatchOTAOptions.vue
@@ -1,0 +1,96 @@
+<template>
+  <form @submit.prevent="sendForm">
+    <FileList
+      v-model="incrementalSources"
+      :disabled="!input.isIncremental"
+      label="Source files"
+    />
+    <v-divider />
+    <FileList
+      v-model="targetBuilds"
+      label="Target files"
+    />
+    <v-divider />
+    <OTAOptions
+      :targetDetails="targetDetails"
+      :targetBuilds="targetBuilds"
+      @update:input="input=$event"
+    />
+    <v-divider class="my-5" />
+    <v-btn
+      block
+      type="submit"
+    >
+      Submit
+    </v-btn>
+  </form>
+</template>
+
+<script>
+import OTAOptions from '@/components/OTAOptions.vue'
+import FileList from '@/components/FileList.vue'
+import { OTAConfiguration } from '@/services/JobSubmission.js'
+
+export default {
+  components: {
+    OTAOptions,
+    FileList,
+  },
+  props: {
+    targetDetails: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      incrementalSources: [],
+      targetBuilds: [],
+      input: new OTAConfiguration(),
+    }
+  },
+  computed: {
+    checkIncremental() {
+      return this.input.isIncremental
+    },
+  },
+  watch: {
+    checkIncremental: {
+      handler: function () {
+        this.$emit('update:isIncremental', this.checkIncremental)
+      },
+    },
+  },
+  created() {
+    this.$emit('update:isIncremental', this.checkIncremental)
+    this.$emit('update:Handler', this.addIncrementalSources, this.addTargetBuilds)
+  },
+  methods: {
+    /**
+     * Send the configuration to the backend.
+     */
+    async sendForm() {
+      try {
+        let response_message = await this.input.sendForms(
+          this.targetBuilds, this.incrementalSources)
+        alert(response_message)
+        this.input.reset()
+      } catch (err) {
+        alert(
+          'Job cannot be started properly for the following reasons: ' + err
+        )
+      }
+    },
+    addIncrementalSources (build) {
+      if (!this.incrementalSources.includes(build)) {
+        this.incrementalSources.push(build)
+      }
+    },
+    addTargetBuilds (build) {
+      if (!this.targetBuilds.includes(build)) {
+        this.targetBuilds.push(build)
+      }
+    }
+  },
+}
+</script>

bluestacks/development-a16

tools/otagui/src/components/BatchOTAOptions.vue
@@ -1,0 +1,96 @@
+<template>
+  <form @submit.prevent="sendForm">
+    <FileList
+      v-model="incrementalSources"
+      :disabled="!otaConfig.isIncremental"
+      label="Source files"
+    />
+    <v-divider />
+    <FileList
+      v-model="targetBuilds"
+      label="Target files"
+    />
+    <v-divider />
+    <OTAOptions
+      :targetDetails="targetDetails"
+      :targetBuilds="targetBuilds"
+      @update:otaConfig="otaConfig=$event"
+    />
+    <v-divider class="my-5" />
+    <v-btn
+      block
+      type="submit"
+    >
+      Submit
+    </v-btn>
+  </form>
+</template>
+
+<script>
+import OTAOptions from '@/components/OTAOptions.vue'
+import FileList from '@/components/FileList.vue'
+import { OTAConfiguration } from '@/services/JobSubmission.js'
+
+export default {
+  components: {
+    OTAOptions,
+    FileList,
+  },
+  props: {
+    targetDetails: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      incrementalSources: [],
+      targetBuilds: [],
+      otaConfig: new OTAConfiguration(),
+    }
+  },
+  computed: {
+    checkIncremental() {
+      return this.otaConfig.isIncremental
+    },
+  },
+  watch: {
+    checkIncremental: {
+      handler: function () {
+        this.$emit('update:isIncremental', this.checkIncremental)
+      },
+    },
+  },
+  created() {
+    this.$emit('update:isIncremental', this.checkIncremental)
+    this.$emit('update:handler', this.addIncrementalSources, this.addTargetBuilds)
+  },
+  methods: {
+    /**
+     * Send the configuration to the backend.
+     */
+    async sendForm() {
+      try {
+        let response_messages = await this.otaConfig.sendForms(
+          this.targetBuilds, this.incrementalSources)
+        alert(response_messages.join('\n'))
+        this.otaConfig.reset()
+      } catch (err) {
+        alert(
+          'Job cannot be started properly for the following reasons: ' + err
+        )
+      }
+    },
+    addIncrementalSources (build) {
+      if (!this.incrementalSources.includes(build)) {
+        this.incrementalSources.push(build)
+      }
+    },
+    addTargetBuilds (build) {
+      if (!this.targetBuilds.includes(build)) {
+        this.targetBuilds.push(build)
+      }
+    }
+  },
+}
+</script>