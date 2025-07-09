0be3aef0fc5d5791ad045dc28fa58e918ae78417
Subhash Reddy Surkanti (xWF) <subhashreddy@google.com>
2025-07-09 07:25:03 -0700

    Revert "Add HSV Viewer support to mechanics demo app"
    This reverts commit ef1ab65e6d2f647ca4a6a2d44e8e855a270a893f.
    
    Reason for revert: Likely culprit for b/430541353  - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    
    Bug: 430541353
    Change-Id: I112451abb92c61897ed3058f3b7fdc00388ec2ab

--- samples/MotionMechanics/src/com/android/mechanics/demo/DemoActivity.kt
+++ samples/MotionMechanics/src/com/android/mechanics/demo/DemoActivity.kt
@@ -23,18 +23,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.google.android.libraries.view.hierarchysnapshotter.HierarchySnapshotterBuilder
-import com.google.android.libraries.view.hierarchysnapshotter.compose.ComposeExtension
 
 class DemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setDecorFitsSystemWindows(false)
-
-        HierarchySnapshotterBuilder.forContext(this)
-            .withCommonAttributeGenerators()
-            .withExtensions(ComposeExtension(this))
-            .buildAndInstall()
 
         setContent {
             MaterialTheme {
